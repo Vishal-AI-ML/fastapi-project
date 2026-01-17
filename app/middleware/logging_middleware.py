aaf8e208e3ce6d9bb90f935acef33d712cb82b80
Vishal Shivhare <vishalshivhare.ai@gmail.com>
2026-01-17 12:22:55 +0530

    added Middleware and Main.py

---
+++ app/middleware/logging_middleware.py
@@ -0,0 +1,12 @@
+from fastapi import Request
+from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
+import logging
+
+from starlette.responses import Response
+
+class LoggingMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request, call_next):
+        logging.info(f'Request:{request.method} {request.url}')
+        response = await call_next(request)
+        logging.info(f'Response:{response.status_code}')
+        return response
